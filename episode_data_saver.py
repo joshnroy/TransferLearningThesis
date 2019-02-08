6f45291b2c627d8e6ac7afc5b60dd3362e3407d4
Josh Roy <joshnroy@gmail.com>
2019-02-08 15:43:06 -0500

    created regression to true state sanity test

--- episode_data_saver.py
+++ episode_data_saver.py
@@ -11,16 +11,16 @@ display.start()
 array = []
 for i_episode in range(0, 1001):
     env = gym.make("cartpole-visual-v1")
-    observation = env.reset()
+    observation, state = env.reset()
     for t in range(100):
         action = env.action_space.sample()
         observation_flat = observation.flatten()
-        array.append(np.insert(observation_flat, 0, action))
-        observation, reward, done, info = env.step(action)
+        array.append(np.concatenate([observation_flat, state]))
+        observation, state, reward, done, info = env.step(action)
         if done:
             if i_episode > 0 and i_episode % 500 == 0:
                 array = np.array(array)
-                name = "test_data/test_data_" + str(i_episode) + ".npy"
+                name = "training_data_with_state/training_data_with_state_" + str(i_episode) + ".npy"
                 np.save(name, array)
                 array = []
                 print("Saved", name)
