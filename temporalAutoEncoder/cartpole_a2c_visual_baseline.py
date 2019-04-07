44e1e36cd654c293b380e5e21a665bbe272b9c62
Josh Roy <joshnroy@gmail.com>
2019-04-07 10:14:34 -0400

    maybe it trains stably now

--- temporalAutoEncoder/cartpole_a2c_visual_baseline.py
+++ temporalAutoEncoder/cartpole_a2c_visual_baseline.py
@@ -5,27 +5,28 @@ import csv
 import numpy as np
 from tqdm import tqdm, trange
 
-from keras.layers import Dense, Conv2D, BatchNormalization, MaxPool2D, Flatten, Activation, concatenate, Input
+from keras.layers import Dense, Conv2D, BatchNormalization, MaxPool2D, Flatten, Activation, concatenate, Input, Lambda
 from keras.models import Sequential, Model, load_model
 from keras.optimizers import Adam
 import keras
 
-import dnnlib
-import dnnlib.tflib as tflib
-import config
 import pickle
 import scipy
+
+from copy import deepcopy
 
 from pyvirtualdisplay import Display
 
 display = Display(visible=0, size=(100, 100))
 display.start()
-keras.losses.custom_loss = keras.losses.mean_squared_error
-
-MODEL_NAME = "reinforcement/AgainHighLowIndependence"
 
 EPISODES = 100000
 RECORD_IMAGES = True
+
+HISTORY_LEN = 5
+
+NUM_FILTERS = 3
+NUM_CONV_LAYERS = 3
 
 
 # A2C(Advantage Actor-Critic) agent for the Cartpole
@@ -40,9 +41,9 @@ class A2CAgent:
         self.value_size = 1
 
         # These are hyper parameters for the Policy Gradient
-        self.discount_factor = 1.
-        self.actor_lr = 1e-4 * 1.
-        self.critic_lr = 1e-4 * 5.
+        self.discount_factor = 0.99
+        self.actor_lr = 1. * 1e-5
+        self.critic_lr = 5. * 1e-5
 
         # create model for policy network
         self.actor = self.build_actor()
@@ -53,87 +54,36 @@ class A2CAgent:
             self.critic.load_weights("./save_model/cartpole_critic.h5")
 
     # approximate policy and value using Neural Network
-    # actor: state is input and probability of each action is output of model
-    def build_actor_thesis(self):
-        # Get the RP extractor pre-trained model
-        encoder_model = load_model(MODEL_NAME)
-        encoder_model = Model(encoder_model.inputs, encoder_model.layers[-2].output)
-        # Should probably do something to identify the non-RP weights here
-        # Freeze Layers
-        for layer in encoder_model.layers:
-            layer.name += "_encoder"
-            layer.trainable = False
-        actor = Dense(512)(encoder_model.outputs[0])
-        actor = Activation('relu')(actor)
-        actor = Dense(24)(actor)
-        actor = Activation('relu')(actor)
-        actor = Dense(self.action_size)(actor)
-        actor = Activation('softmax')(actor)
-        model = Model(inputs=encoder_model.inputs, outputs=actor)
-        model.compile(loss='categorical_crossentropy',
-                      optimizer=Adam(lr=self.actor_lr))
-        model.summary()
-        return model
-
     def build_actor(self):
         # Inputs
-        input1 = Input(shape=(64, 64, 3), name="input1_actor")
-        input2 = Input(shape=(64, 64, 3), name="input2_actor")
-        input3 = Input(shape=(64, 64, 3), name="input3_actor")
+        inputs = [Input(shape=(64, 64, 3), name="input" + str(i) + "_actor") for i in range(HISTORY_LEN)]
 
-
-        # Conv 1
-        conv_layer1 = Conv2D(6, 3);
-        batchnorm_layer1 = BatchNormalization()
-        maxpool_layer1 = MaxPool2D()
-
-        conv1_input_1 = maxpool_layer1(batchnorm_layer1(conv_layer1(input1)))
-        conv1_input_2 = maxpool_layer1(batchnorm_layer1(conv_layer1(input2)))
-        conv1_input_3 = maxpool_layer1(batchnorm_layer1(conv_layer1(input3)))
-
-        # Conv 2
-        conv_layer2 = Conv2D(12, 3);
-        batchnorm_layer2 = BatchNormalization()
-        maxpool_layer2 = MaxPool2D()
-
-        conv2_input_1 = maxpool_layer2(batchnorm_layer2(conv_layer2(conv1_input_1)))
-        conv2_input_2 = maxpool_layer2(batchnorm_layer2(conv_layer2(conv1_input_2)))
-        conv2_input_3 = maxpool_layer2(batchnorm_layer2(conv_layer2(conv1_input_3)))
-
-        # Conv 3
-        conv_layer3 = Conv2D(12, 3);
-        batchnorm_layer3 = BatchNormalization()
-        maxpool_layer3 = MaxPool2D()
-
-        conv3_input_1 = maxpool_layer3(batchnorm_layer3(conv_layer3(conv2_input_1)))
-        conv3_input_2 = maxpool_layer3(batchnorm_layer3(conv_layer3(conv2_input_2)))
-        conv3_input_3 = maxpool_layer3(batchnorm_layer3(conv_layer3(conv2_input_3)))
-
-        # Conv 4
-        # conv_layer4 = Conv2D(24, 3);
-        # batchnorm_layer4 = BatchNormalization()
-        # maxpool_layer4 = MaxPool2D()
-
-        # conv4_input_1 = maxpool_layer4(batchnorm_layer4(conv_layer4(conv3_input_1)))
-        # conv4_input_2 = maxpool_layer4(batchnorm_layer4(conv_layer4(conv3_input_2)))
-        # conv4_input_3 = maxpool_layer4(batchnorm_layer4(conv_layer4(conv3_input_3)))
+        # Convolutional Layers
+        conv = inputs
+        numFilters = NUM_FILTERS
+        for i in range(NUM_CONV_LAYERS):
+            conv_layer = Conv2D(numFilters, 3)
+            batchnorm_layer = BatchNormalization()
+            maxpool_layer = MaxPool2D()
+            conv = [maxpool_layer(batchnorm_layer(conv_layer(x))) for j, x in enumerate(conv)]
+            numFilters *= 2
 
         # Flatten
-        flattened_input_1 = Flatten()(conv3_input_1)
-        flattened_input_2 = Flatten()(conv3_input_2)
-        flattened_input_3 = Flatten()(conv3_input_3)
+        flattened = [Flatten()(x) for x in conv]
 
         # Concatenate
-        concatenated = concatenate([flattened_input_1, flattened_input_2, flattened_input_3])
+        output = concatenate(flattened)
 
-        output = concatenated
-        output = Dense(512, activation='relu', kernel_initializer='he_uniform')(output)
-        output = Dense(256, activation='relu', kernel_initializer='he_uniform')(output)
-        output = Dense(128, activation='relu', kernel_initializer='he_uniform')(output)
-        output = Dense(24, activation='relu', kernel_initializer='he_uniform')(output)
-        output = Dense(self.action_size, activation='softmax', kernel_initializer='he_uniform')(output)
+        # output = Dense(160, activation='relu', kernel_initializer='he_uniform')(output)
+        # output = Dense(96, activation='relu', kernel_initializer='he_uniform')(output)
+        # output = Dense(48, activation='relu', kernel_initializer='he_uniform')(output)
+        output = Dense(24, activation='relu')(output)
+        output = Dense(24, activation='relu')(output)
+        output = Dense(24, activation='relu')(output)
+        output = Dense(self.action_size, activation='softmax')(output)
+        # output = Lambda(lambda x: x * 500)(output)
 
-        actor = Model(inputs = [input1, input2, input3], outputs=[output])
+        actor = Model(inputs = inputs, outputs=[output])
 
         actor.summary()
 
@@ -143,99 +93,88 @@ class A2CAgent:
         return actor
 
     # critic: state is input and value of state is output of model
-    def build_critic_thesis(self):
-        # Get the RP extrcritic pre-trained model
-        encoder_model = load_model(MODEL_NAME)
-        encoder_model = Model(encoder_model.inputs, encoder_model.layers[-2].output)
-        # Should probably do something to identify the non-RP weights here
-        # Freeze Layers
-        for layer in encoder_model.layers:
-            layer.name += "_encoder"
-            layer.trainable = False
-        critic = Dense(512)(encoder_model.outputs[0])
-        critic = Activation('relu')(critic)
-        critic = Dense(24)(critic)
-        critic = Activation('relu')(critic)
-        critic = Dense(self.value_size)(critic)
-        critic = Activation('linear')(critic)
-        model = Model(inputs=encoder_model.inputs, outputs=critic)
-        model.compile(loss="mse", optimizer=Adam(lr=self.critic_lr))
-        model.summary()
-        return model
-
     def build_critic(self):
         # Inputs
-        input1 = Input(shape=(64, 64, 3), name="input1_critic")
-        input2 = Input(shape=(64, 64, 3), name="input2_critic")
-        input3 = Input(shape=(64, 64, 3), name="input3_critic")
+        # inputs = Input(shape=(64, 64, 3), name="input_critic")
 
+        # # Convolutional Layers
+        # numFilters = NUM_FILTERS
+        # conv = inputs
+        # for i in range(NUM_CONV_LAYERS):
+        #     conv_layer = Conv2D(numFilters, 3)
+        #     batchnorm_layer = BatchNormalization()
+        #     maxpool_layer = MaxPool2D()
+        #     conv = maxpool_layer(batchnorm_layer(conv_layer(conv)))
+        #     numFilters *= 2
 
-        # Conv 1
-        conv_layer1 = Conv2D(6, 3);
-        batchnorm_layer1 = BatchNormalization()
-        maxpool_layer1 = MaxPool2D()
+        # Inputs
+        inputs = [Input(shape=(64, 64, 3), name="input" + str(i) + "_actor") for i in range(HISTORY_LEN)]
 
-        conv1_input_1 = maxpool_layer1(batchnorm_layer1(conv_layer1(input1)))
-        conv1_input_2 = maxpool_layer1(batchnorm_layer1(conv_layer1(input2)))
-        conv1_input_3 = maxpool_layer1(batchnorm_layer1(conv_layer1(input3)))
-
-        # Conv 2
-        conv_layer2 = Conv2D(12, 3);
-        batchnorm_layer2 = BatchNormalization()
-        maxpool_layer2 = MaxPool2D()
-
-        conv2_input_1 = maxpool_layer2(batchnorm_layer2(conv_layer2(conv1_input_1)))
-        conv2_input_2 = maxpool_layer2(batchnorm_layer2(conv_layer2(conv1_input_2)))
-        conv2_input_3 = maxpool_layer2(batchnorm_layer2(conv_layer2(conv1_input_3)))
-
-        # Conv 3
-        conv_layer3 = Conv2D(12, 3);
-        batchnorm_layer3 = BatchNormalization()
-        maxpool_layer3 = MaxPool2D()
-
-        conv3_input_1 = maxpool_layer3(batchnorm_layer3(conv_layer3(conv2_input_1)))
-        conv3_input_2 = maxpool_layer3(batchnorm_layer3(conv_layer3(conv2_input_2)))
-        conv3_input_3 = maxpool_layer3(batchnorm_layer3(conv_layer3(conv2_input_3)))
-
-        # Conv 4
-        # conv_layer4 = Conv2D(24, 3);
-        # batchnorm_layer4 = BatchNormalization()
-        # maxpool_layer4 = MaxPool2D()
-
-        # conv4_input_1 = maxpool_layer4(batchnorm_layer4(conv_layer4(conv3_input_1)))
-        # conv4_input_2 = maxpool_layer4(batchnorm_layer4(conv_layer4(conv3_input_2)))
-        # conv4_input_3 = maxpool_layer4(batchnorm_layer4(conv_layer4(conv3_input_3)))
+        # Convolutional Layers
+        conv = inputs
+        numFilters = NUM_FILTERS
+        for i in range(NUM_CONV_LAYERS):
+            conv_layer = Conv2D(numFilters, 3)
+            batchnorm_layer = BatchNormalization()
+            maxpool_layer = MaxPool2D()
+            conv = [maxpool_layer(batchnorm_layer(conv_layer(x))) for j, x in enumerate(conv)]
+            numFilters *= 2
 
         # Flatten
-        flattened_input_1 = Flatten()(conv3_input_1)
-        flattened_input_2 = Flatten()(conv3_input_2)
-        flattened_input_3 = Flatten()(conv3_input_3)
+        flattened = [Flatten()(x) for x in conv]
 
         # Concatenate
-        concatenated = concatenate([flattened_input_1, flattened_input_2, flattened_input_3])
+        output = concatenate(flattened)
 
-        output = concatenated
-        output = Dense(512, activation='relu', kernel_initializer='he_uniform')(output)
-        output = Dense(256, activation='relu', kernel_initializer='he_uniform')(output)
-        output = Dense(128, activation='relu', kernel_initializer='he_uniform')(output)
-        output = Dense(24, activation='relu', kernel_initializer='he_uniform')(output)
-        output = Dense(self.value_size, activation='linear', kernel_initializer='he_uniform')(output)
+        # output = Dense(160, activation='relu', kernel_initializer='he_uniform')(output)
+        # output = Dense(96, activation='relu', kernel_initializer='he_uniform')(output)
+        # output = Dense(48, activation='relu', kernel_initializer='he_uniform')(output)
+        # output = Dense(24, activation='relu', kernel_initializer='he_uniform')(output)
+        output = Dense(24, activation='relu')(output)
+        output = Dense(24, activation='relu')(output)
+        output = Dense(24, activation='relu')(output)
+        output = Dense(self.value_size, activation='linear')(output)
 
-        critic = Model(inputs = [input1, input2, input3], outputs=[output])
+        # output = Lambda(lambda x: x * 300)(output)
+
+        critic = Model(inputs = inputs, outputs=[output])
 
         critic.summary()
+
         critic.compile(loss="mse", optimizer=Adam(lr=self.critic_lr))
         return critic
 
     # using the output of policy network, pick action stochastically
     def get_action(self, states_history):
-        policy = self.actor.predict(states_history, batch_size=1).flatten()
-        if np.isnan(policy).any():
-            print("Policy has a nan")
+        if np.isnan(states_history).any():
+            print("States history has a nan")
             sys.exit()
-        # print(policy)
-        choice = np.random.choice(self.action_size, 1, p=policy)[0]
-        # print(choice, policy)
+        policy = self.actor.predict(states_history)[0]
+        # if np.isnan(policy).any():
+        #     print(policy)
+        #     print("Policy has a nan")
+        # #     sys.exit()
+        # if (np.sum(policy) <= 0 or np.isnan(policy).any()):
+        #     normalized_policy = np.asarray([0.5, 0.5])
+        # else:
+        #     normalized_policy = policy / np.sum(policy)
+        if np.isnan(policy).any():
+            print("Concern")
+            policy = [0.5, 0.5]
+        softmax_policy = scipy.special.expit(policy) / np.sum(scipy.special.expit(policy))
+        # if np.isnan(softmax_policy).any():
+        #     print("policy", normalized_policy)
+        #     print("softmax_policy", softmax_policy)
+        #     print("exp(policy)", np.exp(normalized_policy))
+        #     print("sum(exp(policy))", np.sum(np.exp(normalized_policy)))
+        #     print("softmax_policy has a nan")
+        #     softmax_policy = np.asarray([0.5, 0.5])
+        #     # softmax_policy[(np.isfinite(softmax_policy))] = 0
+        #     # softmax_policy[np.isnan(softmax_policy)] = 1
+        #     # print("fixed softmax_policy", softmax_policy)
+        #     # sys.exit()
+        choice = np.random.choice(self.action_size, 1, p=softmax_policy)[0]
+        # choice = np.argmax(policy)
         return choice
 
     # update policy network every episode
@@ -244,10 +183,14 @@ class A2CAgent:
         advantages = np.zeros((1, self.action_size))
 
         value = self.critic.predict(states_history)[0]
+        value = np.clip(value, -100, 500)
+        assert(not np.isnan(value).any())
 
         next_states_history = states_history[1:]
         next_states_history.append(next_state)
         next_value = self.critic.predict(next_states_history)[0]
+        next_value = np.clip(next_value, -100, 500)
+        assert(not np.isnan(next_value).any())
 
         if done:
             advantages[0][action] = reward - value
@@ -255,12 +198,23 @@ class A2CAgent:
         else:
             advantages[0][action] = reward + self.discount_factor * (next_value) - value
             target[0][0] = reward + self.discount_factor * next_value
+        softmax_advantages = advantages / np.sum(advantages)
 
-        self.actor.fit(states_history, advantages, epochs=1, verbose=0)
-        self.critic.fit(states_history, target, epochs=1, verbose=0)
+        assert(not np.isnan(states_history).any())
+        # print(type(states_history), len(states_history), states_history[0].shape)
+        actor_history = self.actor.fit(states_history, advantages, epochs=1, verbose=0)
+        critic_history = self.critic.fit(states_history, target, epochs=1, verbose=0)
+
+        print("critic", value, target, critic_history.history)
+        actor_output = self.actor.predict(states_history)
+        print("actor", actor_output, advantages, actor_history.history['loss'][0])
+        assert(not np.isnan(actor_output).any())
+
+        return actor_history.history['loss'][0], critic_history.history['loss'][0]
 
 
 if __name__ == "__main__":
+
     # In case of CartPole-v1, maximum length of episode is 500
     env = gym.make('cartpole-visual-v1')
     # get size of state and action from environment
@@ -272,7 +226,7 @@ if __name__ == "__main__":
 
     scores, episodes = [], []
 
-    with open('save_graph/visual_baseline_history_more_dense3.csv', 'w') as csvfile:
+    with open('save_graph/visual_baseline5.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
         for e in trange(EPISODES):
             states_history = []
@@ -280,19 +234,22 @@ if __name__ == "__main__":
             done = False
             score = 0
             state = env.reset()
+            state = state / 255.
             state = np.reshape(state, (1,) + state.shape)
 
-
             i = 0
+            actor_losses = []
+            critic_losses = []
             while not done:
                 if agent.render:
                     env.render()
 
-                if len(states_history) >= 3:
+                if len(states_history) >= HISTORY_LEN:
                     states_history.pop(0)
                     states_history.append(state)
                     action = agent.get_action(states_history)
                     next_state, reward, done, info = env.step(action)
+                    next_state = next_state / 255.
 
 
                     next_state = np.reshape(next_state, (1,) + next_state.shape)
@@ -300,7 +257,9 @@ if __name__ == "__main__":
                     # if an action make the episode end, then gives penalty of -100
                     reward = reward if not done or score == 499 else -100
 
-                    agent.train_model(states_history, action, reward, next_state, done)
+                    actor_loss, critic_loss = agent.train_model(states_history, action, reward, next_state, done)
+                    actor_losses.append(actor_loss)
+                    critic_losses.append(critic_loss)
 
                     score += reward
                     state = next_state
@@ -308,6 +267,7 @@ if __name__ == "__main__":
                     states_history.append(state)
                     action = np.random.choice(agent.action_size, 1)[0]
                     next_state, reward, done, info = env.step(action)
+                    next_state = next_state / 255.
 
                     next_state = np.reshape(next_state, (1,) + next_state.shape)
 
@@ -322,8 +282,11 @@ if __name__ == "__main__":
                     score = score if score == 500.0 else score + 100
                     scores.append(score)
                     episodes.append(e)
-                    writer.writerow([str(e), str(score)])
+                    writer.writerow([str(e), str(score), str(np.mean(actor_losses)), str(np.mean(critic_losses))])
                     csvfile.flush()
+
+                    actor_losses = []
+                    critic_losses = []
 
                     # if the mean of scores of last 10 episode is bigger than 490
                     # stop training
--- temporalAutoEncoder/results_plotter.jl
+++ temporalAutoEncoder/results_plotter.jl
@@ -2,7 +2,7 @@ using Plots
 using CSV
 using DataFrames
 
-csvfile = CSV.read("save_graph/visual_darla4_history10.csv")
+csvfile = CSV.read("save_graph/visual_baseline5.csv")
 x = csvfile[:1]
 y = csvfile[:2]
 
@@ -15,7 +15,7 @@ for i in 2:length(y)
 end
 multiple = [y smoothed]
 
-plot(x, multiple, title="DARLA Visual History A2C", linealpha = [0.5 1], label=["Raw Scores" "Smoothed Scores"])
+plot(x, multiple, title="Baseline Visual History A2C", linealpha = [0.5 1], label=["Raw Scores" "Smoothed Scores"])
 #= plot(x, y, title="S Encoder A2C", label=["Scores"]) =#
 
-savefig("save_graph/visual_darla4_history10.png")
+savefig("save_graph/visual_baseline5.png")
