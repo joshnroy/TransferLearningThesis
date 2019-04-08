78c5128cbd2d421e8fea4c101e10f2f6d70334e6
Josh Roy <joshnroy@gmail.com>
2019-04-08 10:20:25 -0400

    added velocity baseline test

--- temporalAutoEncoder/results_plotter.jl
+++ temporalAutoEncoder/results_plotter.jl
@@ -6,7 +6,7 @@ using Plots
 using CSV
 using DataFrames
 
-csvfile = CSV.read("save_graph/visual_darla4.csv")
+csvfile = CSV.read("save_graph/velocity_baseline1.csv")
 x = csvfile[:1]
 y = csvfile[:2]
 actor_reward = csvfile[:3]
@@ -23,14 +23,16 @@ for i in 2:length(actor_reward)
     smoothed_actor_reward[i] = alpha * actor_reward[i] + (1. - alpha) * smoothed_actor_reward[i-1]
     smoothed_critic_reward[i] = alpha * critic_reward[i] + (1. - alpha) * smoothed_critic_reward[i-1]
 end
+clamp!(smoothed_actor_reward, 0, 500)
+clamp!(smoothed_critic_reward, 0, 500)
 #= multiple = [y smoothed] =#
 #= rewards = [actor_reward smoothed_actor_reward critic_reward smoothed_critic_reward] =#
-rewards = [smoothed_actor_reward * 10 smoothed_critic_reward][250:length(smoothed_actor_reward), :]
+rewards = [smoothed_actor_reward * 10 smoothed_critic_reward]
 #= rewards = [smoothed_actor_reward * 10 smoothed_critic_reward] =#
 #= rewards = [smoothed_actor_reward] =#
 
 #= plot(x, multiple, title="DARLA Visual History A2C", linealpha = [0.5 1], label=["Raw Scores" "Smoothed Scores"]) =#
-plot(x[250:length(smoothed_actor_reward)], rewards, title="DARLA Visual History A2C (loss)", linealpha = [1 1], label=["Smoothed Actor Loss" "Smoothed Critic Loss"])
+plot(x, rewards, title="Baseline Visual History A2C (loss)", linealpha = [1 1], label=["Smoothed Actor Loss" "Smoothed Critic Loss"])
 #= plot(x, rewards, title="DARLA Visual History A2C (loss)", linealpha = [1], label=["Smoothed Actor Loss"]) =#
 
-savefig("save_graph/visual_darla4_rewards.png")
+savefig("save_graph/velocity_baseline1_rewards.png")
