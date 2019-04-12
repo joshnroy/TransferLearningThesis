b02594a5b85f6386da5a3f0abe64293fa7d28cc6
Josh Roy <joshnroy@gmail.com>
2019-04-12 01:02:51 -0400

    some stuff

--- ddqn/vae_visual_cartpole.py
+++ ddqn/vae_visual_cartpole.py
@@ -3,7 +3,7 @@ import gym
 import gym_cartpole_visual
 
 from keras.models import Sequential, Model
-from keras.layers import Dense, Activation, Flatten, Input, Conv2D, BatchNormalization, MaxPool2D, Reshape, Lambda, concatenate
+from keras.layers import Dense, Activation, Flatten, Input, Conv2D, BatchNormalization, MaxPool2D, Reshape, Lambda, concatenate, Dropout
 from keras.optimizers import Adam
 
 from rl.agents.dqn import DQNAgent
@@ -15,6 +15,8 @@ from pyvirtualdisplay import Display
 
 from variational_autoencoder_deconv import vae
 
+import matplotlib.pyplot as plt
+
 display = Display(visible=0, size=(100, 100))
 display.start()
 
@@ -23,7 +25,7 @@ WEIGHTS_FILE = "vae_cnn_cartpole.h5"
 ENV_NAME = 'cartpole-visual-v1'
 NUM_FILTERS = 6
 NUM_CONV_LAYERS = 3
-NUM_HIDDEN_LAYERS = 3
+NUM_HIDDEN_LAYERS = 5
 HIDDEN_LAYER_SIZE = 48
 
 
@@ -68,15 +70,21 @@ policy = BoltzmannQPolicy()
 # you can specify the dueling_type to one of {'avg','max','naive'}
 dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=100,
                enable_dueling_network=True, dueling_type='avg', target_model_update=1e-2, policy=policy)
-dqn.compile(Adam(lr=1e-5), metrics=['mae'])
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
+history = dqn.fit(env, nb_steps=500000, visualize=False, verbose=1)
+np.savez_compressed("vae_dqn_training_history_100k", episode_reward=np.asarray(history.history['episode_reward']))
 
 # After training is done, we save the final weights.
-dqn.save_weights('duel_dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+dqn.save_weights('vae_dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
-dqn.test(env, nb_episodes=5, visualize=False)
+print("Test on original colors")
+dqn.test(env, nb_episodes=10, visualize=False)
+
+print("Test on changed colors")
+env.change_color()
+dqn.test(env, nb_episodes=10, visualize=False)
