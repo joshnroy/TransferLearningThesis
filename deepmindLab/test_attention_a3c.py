e4899f378a0c49c5d9a694ad18a6631dfe7c036a
Josh Roy <joshnroy@gmail.com>
2019-09-19 10:57:17 -0400

    adding test

---
+++ deepmindLab/test_attention_a3c.py
@@ -0,0 +1,51 @@
+import numpy as np
+import tensorflow as tf
+
+import gym, time, random, threading, sys
+from tqdm import trange
+import csv
+import matplotlib.pyplot as plt
+import cv2
+
+from skimage import io
+
+
+from keras.models import *
+from keras.layers import *
+from keras import backend as K
+
+from seekavoid_gymlike_wrapper import SeekAvoidEnv
+
+import deepmind_lab
+
+from attention_a3c import Brain
+
+def main():
+    b = Brain(test=True)
+    env = SeekAvoidEnv(test=True)
+
+    e_rewards = []
+    episodes = 300
+
+    save_folder = "episode_images/"
+
+    for i in trange(episodes):
+        j = 0
+        done = False
+        obs = env.reset()
+        obs = np.expand_dims(obs, axis=0)
+        e_reward = 0.
+        while not done:
+            prediction, attention_weights = b.predict_p(obs)[0]
+            action = np.argmax(prediction)
+            obs, reward, done, _ = env.step(action)
+            obs = np.expand_dims(obs, axis=0)
+            e_reward += reward
+            j += 1
+        e_rewards.append(e_reward)
+    print(np.mean(e_rewards), np.std(e_rewards), np.max(e_rewards), np.min(e_rewards))
+    # plt.hist(e_rewards, int(np.round(np.max(e_rewards))))
+    # plt.show()
+
+if __name__ == "__main__":
+    main()
