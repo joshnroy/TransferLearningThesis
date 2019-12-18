8544eb032d3761ab3b5f19e44b34c0a1badb6a6f
Josh Roy <joshnroy@gmail.com>
2019-12-18 13:14:49 -0500

    done for now

--- new/betavae.py
+++ new/betavae.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
-from klepto.archives import *
 from tqdm import tqdm
 
 from torch.utils.data import DataLoader
@@ -16,13 +15,12 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class Encoder(nn.Module):
     def __init__(self, input_dim, hidden_dim, z_dim):
-        super().__init__()
+        super(Encoder, self).__init__()
 
-        self.c1 = nn.Conv2d(3, 32, 4, stride=2, padding=1)
-        self.c2 = nn.Conv2d(32, 32, 4, stride=2, padding=1)
-        self.c3 = nn.Conv2d(32, 64, 4, stride=2, padding=1)
-        self.c4 = nn.Conv2d(64, 64, 4, stride=2, padding=1)
-        self.linear = nn.Linear(64 * 6 * 6, hidden_dim)
+        self.c1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
+        self.c2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.c3 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
+        self.linear = nn.Linear(64 * 4 * 4, hidden_dim)
         self.mu = nn.Linear(hidden_dim, z_dim)
         self.var = nn.Linear(hidden_dim, z_dim)
 
@@ -30,7 +28,7 @@ class Encoder(nn.Module):
         hidden = F.relu(self.c1(x))
         hidden = F.relu(self.c2(hidden))
         hidden = F.relu(self.c3(hidden))
-        hidden = F.relu(self.c4(hidden)).reshape(-1, 64 * 6 * 6)
+        hidden  = hidden.reshape(-1, 64 * 4 * 4)
         hidden = F.relu(self.linear(hidden))
         z_mu = self.mu(hidden)
         z_var = self.var(hidden)
@@ -39,38 +37,35 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
     def __init__(self, z_dim, hidden_dim, output_dim):
-        super().__init__()
+        super(Decoder, self).__init__()
 
         self.linear1 = nn.Linear(z_dim, hidden_dim)
-        self.linear2 = nn.Linear(hidden_dim, 64 * 5 * 5)
-        self.c1 = nn.ConvTranspose2d(64, 64, 4, stride=2)
-        self.c2 = nn.ConvTranspose2d(64, 32, 4, stride=2)
-        self.c3 = nn.ConvTranspose2d(32, 32, 4, stride=2)
-        self.c4 = nn.ConvTranspose2d(32, 3, 4, stride=2)
+        self.linear2 = nn.Linear(hidden_dim, 64 * 4 * 4)
+        self.c1 = nn.ConvTranspose2d(64, 32, 3, stride=2)
+        self.c2 = nn.ConvTranspose2d(32, 32, 3, stride=2)
+        self.c3 = nn.ConvTranspose2d(32, 3, 3, stride=2)
 
     def forward(self, x):
         hidden = F.relu(self.linear1(x))
-        hidden = F.relu(self.linear2(hidden)).reshape(-1, 64, 5, 5)
+        hidden = F.relu(self.linear2(hidden)).reshape(-1, 64, 4, 4)
         hidden = F.relu(self.c1(hidden))
         hidden = F.relu(self.c2(hidden))
-        hidden = F.relu(self.c3(hidden))
-        predicted = torch.sigmoid(self.c4(hidden))[:, :, :96, :96]
+        predicted = torch.sigmoid(self.c3(hidden))[:, :, :32, :32]
 
         return predicted
 
 class BetaVAE(nn.Module):
     def __init__(self, input_dim):
-        super().__init__()
+        super(BetaVAE, self).__init__()
 
         self.hidden_dim = 256
         self.z_dim = 32
 
-        self.alpha = 0.
         self.beta = 1.
-        self.lr = 1e-4
+        self.lr = 5e-4
 
-        self.batch_size = 32
-        self.n_epochs = 10
+        self.batch_size = 100
+        self.n_epochs = 30
 
         self.enc = Encoder(input_dim, self.hidden_dim, self.z_dim)
         self.enc.to(device)
@@ -96,11 +91,7 @@ class BetaVAE(nn.Module):
 
         kl_loss = 0.5 * torch.sum(torch.exp(z_var) + z_mu**2 - 1.0 - z_var)
 
-        s_norm = torch.mean((x[1:, :] - x[:-1, :])**2)
-        phi_norm = torch.mean((z_sample[1:, :] - z_sample[:-1, :])**2)
-        lip_loss = phi_norm / s_norm
-
-        return recon_loss + self.beta * kl_loss + self.alpha * lip_loss
+        return recon_loss + self.beta * kl_loss
 
     def train_model(self, train_dataset, save_name):
         self.train()
